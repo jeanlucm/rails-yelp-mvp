954600dc1ea2fc181799e2e7174946da094a9ca0
jeanluc <contact@forummc.com>
2017-11-23 09:38:43 +0000

    r

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root_to 'restaurants#index'
+  root to: 'restaurants#index'
   # namespace :admin do
   resources :restaurants
   # , only: [:index]
